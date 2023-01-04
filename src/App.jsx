7a94b2de12b2912619736234ab354b76944194a8
German Gauna <germangauna88@gmail.com>
2023-01-03 22:15:30 -0300

    creamos carpetas

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import NavBar from './components/NavBar/Index'
+
+function App() {
+	return (
+		<NavBar/>
+	);
+	
+}
+export default App;
---
+++ src/components/NavBar/Index.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export const NavBar = () => {
+    return (
+        
+        <h1>NavBar</h1>
+    );
+}
+
+export default NavBar;
